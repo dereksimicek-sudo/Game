2b625a59e81b01959c95885f532f6011c5094a23
dereksimicek-sudo <derek.simicek@tria-tr.cz>
2026-03-02 08:41:45 +0100

    Delete Old directory

--- Old/5.py
+++
@@ -1,9 +0,0 @@
-def faktorial(n):
-    if n <= 1:
-        return 1
-    else:
-        return n * faktorial(n - 1)
-
-
-n = 8
-print(f"faktorial čísla{n} je roven: {faktorial(n)}")
--- Old/tist.py
+++
@@ -1,6 +0,0 @@
-prazdny_list = []
-for i in range(10):
-    prazdny_list.append(i*10)
-    prazdny_list.remove(5)
-
-print(prazdny_list)
